92f7cdd62f838b6b7d9993799ce270c44e9b256c
nguyentrunghieu <hieunguyen3092003@gmail.com>
2025-03-25 13:38:48 +0700

    complete lab1/ex1

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,29 @@
 #include <Arduino.h>
 
+void printHelloWorld(void *pvParameters)
+{
+  while (true)
+  {
+    Serial.println("Hello World!");
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
+
+  xTaskCreate(
+      printHelloWorld,  // Function to execute
+      "HelloWorldTask", // Task name (for debugging)
+      10000,            // Stack size in bytes (adjust if needed)
+      NULL,             // Task parameters (none in this case)
+      1,                // Task priority (1 is low, higher numbers are higher priority)
+      NULL              // Task handle (not used here)
+  );
 }
+
 void loop()
 {
-  Serial.println("Hello World");
-  delay(1000);
+  // Empty because we are using FreeRTOS
 }
